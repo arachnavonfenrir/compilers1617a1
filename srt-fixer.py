3ac4dd30e501b0ce6c80c9f48a87b5d9fc2cac86
arachnavonfenrir <vladaklyma@gmail.com>
2017-05-04 22:03:11 +0300

    Update srt-fixer.py

--- srt-fixer.py
+++ srt-fixer.py
@@ -2,7 +2,7 @@
 import sys
 import re
 import argparse
-
+import datetime
 
 parser = argparse.ArgumentParser()
 # add mandatory (positional) arguments
@@ -13,11 +13,22 @@ parser.add_argument("offset",type=float,help="subtitle offset in seconds to appl
 args = parser.parse_args()
 
 with open(args.fname,newline='') as ifp:	
-	for line in ifp:
+	h=args.offset/3600
+	tmp=args.offset%3600
+	minute=tmp/60
+	s=tmp%60
 	
+	for line in ifp:
+		
 		# -- αντικαταστήστε με τον δικό σας κώδικα (αρχή) --
-
-		sys.stdout.write(line)
+		if date is None
+			sys.stdout.write(line)
 
 		# -- αντικαταστήστε με τον δικό σας κώδικα (τέλος) --
-
+		
+			
+			d1= datetime.datetime(h2+h,min2+minute,sec2+s)
+			'{%H:%M:%S}'.format(d1)
+			d2= datetime.datetime(h2+h,min2+minute,sec2+s)
+			'{%H:%M:%S}'.format(d2)
+			sys.stdout.write(d1 --> d2)
