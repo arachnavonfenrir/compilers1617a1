41e2ed54944b4620ec5b1cafec951b2fe88f0b4e
arachnavonfenrir <vladaklyma@gmail.com>
2017-05-07 22:12:34 +0300

    Update srt-fixer.py

--- srt-fixer.py
+++ srt-fixer.py
@@ -19,16 +19,22 @@ with open(args.fname,newline='') as ifp:
 	s=tmp%60
 	
 	for line in ifp:
-		
+		rexp=re.compile(([0-9]+):([0-9]+):([0-9]+),([0-9]+)/s-->/s([0-9]+):([0-9]+):([0-9]+),([0-9]+))
+		date=rexp.search(line)
 		# -- αντικαταστήστε με τον δικό σας κώδικα (αρχή) --
 		if date is None
 			sys.stdout.write(line)
 
 		# -- αντικαταστήστε με τον δικό σας κώδικα (τέλος) --
 		
-			
+			h2=int(date.group(1))
+			min2=int(date.group(2))
+			sec2=float(date.group(3))+float(date.group(4))/1000
+			h2=int(date.group(6))
+			min2=int(date.group(7))
+			sec2=float(date.group(8))+float(date.group(9))/1000
 			d1= datetime.datetime(h2+h,min2+minute,sec2+s)
 			'{%H:%M:%S}'.format(d1)
 			d2= datetime.datetime(h2+h,min2+minute,sec2+s)
 			'{%H:%M:%S}'.format(d2)
-			sys.stdout.write(d1 --> d2)
+			sys.stdout.write(d1,-->, d2)
