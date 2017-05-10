43a33625c1a9eeaf808d311a9634825bc18363fd
arachnavonfenrir <vladaklyma@gmail.com>
2017-05-10 15:38:11 +0300

    Update srt-fixer.py

--- srt-fixer.py
+++ srt-fixer.py
@@ -19,24 +19,29 @@ with open(args.fname,newline='') as ifp:
 	s=tmp%60
 	
 	for line in ifp:
-		rexp=re.compile(([0-9]+):([0-9]+):([0-9]+),([0-9]+)/s-->/s([0-9]+):([0-9]+):([0-9]+),([0-9]+))
+		rexp=re.compile("([0-9]+):([0-9]+):([0-9]+),([0-9]+)/s-->/s([0-9]+):([0-9]+):([0-9]+),([0-9]+)")
 		date=rexp.search(line)
 		# -- αντικαταστήστε με τον δικό σας κώδικα (αρχή) --
 		if date is None
 			sys.stdout.write(line)
 
 		# -- αντικαταστήστε με τον δικό σας κώδικα (τέλος) --
-		
-			h2=int(date.group(1))
-			min2=int(date.group(2))
-			sec2=float(date.group(3))+float(date.group(4))/1000
-			h2=int(date.group(6))
-			min2=int(date.group(7))
-			sec2=float(date.group(8))+float(date.group(9))/1000
-			d1= datetime.datetime(h2+h,min2+minute,sec2+s)
+			sec2=float(date.group(3))+float(date.group(4))/1000+s
+			min2=int(date.group(2))+minute+sec2/60
+			h2=int(date.group(1))+h+min2/60
+			sec2=sec2%60
+			min2=min2%60
+			sec3=float(date.group(8))+float(date.group(9))/1000+s
+			min3=int(date.group(7))+sec3/60+minute
+			h3=int(date.group(6))+h+min3/60
+			sec3=sec3%60
+			min3=min3%60			
+			
+			d1= datetime.datetime(h2,min2,sec2)
 			'{%H:%M:%S}'.format(d1)
-			d2= datetime.datetime(h2+h,min2+minute,sec2+s)
+			
+			d2= datetime.datetime(h2,min2,sec2)
 			'{%H:%M:%S}'.format(d2)
 			re.sub('.',',',d1)
 			re.sub('.',',',d2)
-			sys.stdout.write(d1,-->, d2)
+			sys.stdout.write(d1," --> ", d2)
